c4e36826f130f2c3be064329443ed4f55401b114
Inzamam Malik <malikasinger1@gmail.com>
2017-06-10 15:04:52 +0500

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ExpandableListModule } from 'angular2-expandable-list';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ExpandableListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
